000551fb7b0d9d1f890e5d36420a5485a7b8756f
Anwar <anwmalik@gmail.com>
2018-03-11 17:51:25 +0800

    added model for registration info

--- RegisterationApp/Models/RegisterationInfo.cs
+++ RegisterationApp/Models/RegisterationInfo.cs
@@ -1,9 +1,27 @@
-﻿namespace RegisterationApp.Models
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegisterationApp.Models
 {
     public class RegisterationInfo
     {
-        public RegisterationInfo()
-        {
-        }
+        [Required(ErrorMessage = "Please enter BSB Number.")]
+        [RegularExpression("^[0-9]{6}$", ErrorMessage = "The BSB number is invalid. Minimum 6 number is required.")]
+        [JsonProperty("bsb")]
+        public string BankStateBranchCode { get; set; }
+        [Required(ErrorMessage = "Please enter account number.")]
+        [RegularExpression("^[0-9]{8}$", ErrorMessage = "Please enter account number with invalid amount.")]
+        [JsonProperty("accountNumber")]
+        public string AccountNumber { get; set; }
+        [Required(ErrorMessage = "Please enter the Account Name")]
+        [JsonProperty("accountName")]
+        public string AccountName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Reference")]
+        [JsonProperty("reference")]
+        public string Reference { get; set; }
+        [Required(ErrorMessage = "Please enter the Amount")]
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
     }
 }
---
+++ RegistrationApp.Tests/RegistrationInfoModelTests.cs
@@ -0,0 +1,109 @@
+﻿using RegisterationApp.Models;
+using Shouldly;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Registerations.Test
+{
+    public class RegistrationInfoModelTests
+    {
+        [Fact]
+        public void Valiedate_Model_Given_Valid_Model_ExpectNoValidationErrors()
+        {
+            var model = new RegisterationInfo();
+            model.BankStateBranchCode = "123456";
+            model.AccountNumber = "11112222";
+            model.AccountName = "Test";
+            model.Reference = "test";
+            model.Amount = 40m;
+
+            var results = ValidateModel(model);
+
+            results.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void ValidateModel_Given_BSB_IsNull_ExpectBsbRequiredError()
+        {
+            var model = new RegisterationInfo();
+            model.AccountNumber = "11112222";
+            model.AccountName = "Test";
+            model.Reference = "test";
+            model.Amount = 40m;
+
+            var results = ValidateModel(model);
+
+            results.Count.ShouldBe(1);
+            results.First().ErrorMessage.ShouldBe("Please enter BSB Number.");
+            results.First().MemberNames.First().ShouldBe(nameof(RegisterationInfo.BankStateBranchCode));
+        }
+
+        [Fact]
+        public void ValidateModel_Given_BSB_HasInValid_ExpectBsbRequiredError()
+        {
+            var model = new RegisterationInfo
+            {
+                BankStateBranchCode = "12346",
+                AccountNumber = "11112222",
+                AccountName = "Test",
+                Reference = "test",
+                Amount = 40m
+            };
+
+            var results = ValidateModel(model);
+
+            results.Count.ShouldBe(1);
+            results.First().ErrorMessage.ShouldBe("The BSB number is invalid. Minimum 6 number is required.");
+            results.First().MemberNames.First().ShouldBe(nameof(RegisterationInfo.BankStateBranchCode));
+        }
+
+
+        [Fact]
+        public void ValidateModel_Given_AccountNo_IsNull_ExpectAccountNumberRequiredError()
+        {
+            var model = new RegisterationInfo
+            {
+                BankStateBranchCode = "123456",
+                AccountName = "Test",
+                Reference = "test",
+                Amount = 40m
+            };
+
+            var results = ValidateModel(model);
+
+            results.Count.ShouldBe(1);
+            results.Last().ErrorMessage.ShouldBe("Please enter account number.");
+            results.Last().MemberNames.First().ShouldBe(nameof(RegisterationInfo.AccountNumber));
+        }
+
+        [Fact]
+        public void ValidateModel_Given_Account_HasInValid_ExpectAccountNumberInValidError()
+        {
+            var model = new RegisterationInfo
+            {
+                BankStateBranchCode = "123456",
+                AccountNumber = "1111 222",
+                AccountName = "Test",
+                Reference = "test",
+                Amount = 40m
+            };
+
+            var results = ValidateModel(model);
+
+            results.Count.ShouldBe(1);
+            results.First().ErrorMessage.ShouldBe("Please enter account number with invalid amount.");
+            results.First().MemberNames.First().ShouldBe(nameof(RegisterationInfo.AccountNumber));
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+    }
+
+}
