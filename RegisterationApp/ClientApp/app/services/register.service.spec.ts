52ff624c80ee9c8695ced7b8979a565f81790f42
Anwar <anwmalik@gmail.com>
2018-03-11 18:43:35 +0800

    register typescript services for registration

---
+++ RegisterationApp/ClientApp/app/services/register.service.spec.ts
@@ -0,0 +1,60 @@
+﻿import { TestBed, getTestBed, async, inject } from '@angular/core/testing';
+import {
+    HttpModule,
+    Http,
+    Response,
+    ResponseOptions,
+    XHRBackend,
+    RequestMethod
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { RegisterService } from './register.service';
+import { RegistrationInfo } from '../models/register.info';
+import { BaseRequestOptions } from '@angular/http'
+import { } from 'jasmine'
+
+describe('Register Service', () => {
+    let mockBackend: MockBackend;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                RegisterService,
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory:
+                    (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backend, defaultOptions);
+                    }
+                }
+            ],
+            imports: [
+                HttpModule
+            ]
+        });
+
+        mockBackend = getTestBed().get(MockBackend);
+
+    }));
+
+    it('should be able to register the information',
+
+        async(inject([RegisterService], (registerService: RegisterService) => {
+            mockBackend.connections.subscribe((connection: MockConnection) => {
+                expect(connection.request.method).toBe(RequestMethod.Post);
+                connection.mockRespond(new Response(new ResponseOptions({ status: 200 })));
+            });
+
+            let data: RegistrationInfo = new RegistrationInfo(222222, 111222333, 'adfasf', 'adfas', 22.0);
+            registerService.register(data).subscribe(
+                (successResult) => {
+                    expect(successResult).toBeDefined();
+                    expect(successResult.status).toBe(200);
+                });
+        }))
+    );
+
+});  
---
+++ RegisterationApp/ClientApp/app/services/register.service.ts
@@ -0,0 +1,16 @@
+﻿import { Injectable } from '@angular/core';
+import { RegistrationInfo } from '../models/register.info';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class RegisterService {
+
+    constructor(private http: Http) { }
+
+    register(register: RegistrationInfo): Observable<Response> {
+
+        return this.http.post('/api/register', register);
+    }
+}
