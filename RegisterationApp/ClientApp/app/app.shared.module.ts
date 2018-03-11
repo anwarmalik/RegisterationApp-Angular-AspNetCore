9e78f0c2776927b87caf36a08654b9320967499c
Anwar <anwmalik@gmail.com>
2018-03-11 19:40:02 +0800

    reference the components in app module

--- RegisterationApp/ClientApp/app/app.shared.module.ts
+++ RegisterationApp/ClientApp/app/app.shared.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterService } from './services/register.service';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        HomeComponent
+        HomeComponent,
+        RegisterComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
-        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'register', component: RegisterComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        RegisterService
     ]
 })
 export class AppModuleShared {
