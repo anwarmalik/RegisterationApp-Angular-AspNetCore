2f316389ed1b386a48be17cf289e8696c47f976d
Anwar <anwmalik@gmail.com>
2018-03-11 17:37:52 +0800

    added controller

---
+++ RegisterationApp/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegisterationApp.Interfaces;
+using RegisterationApp.Models;
+
+namespace RegisterationApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Register")]
+    public class RegisterController : Controller
+    {
+        private IRegisterationService service;
+
+        public RegisterController(IRegisterationService service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Post(RegisterationInfo registerationInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            service.Save(registerationInfo);
+            return Ok();
+        }
+
+    }
+}
---
+++ RegistrationApp.Tests/RegisterControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using RegisterationApp.Controllers;
+using RegisterationApp.Interfaces;
+using RegisterationApp.Models;
+using Shouldly;
+using Xunit;
+
+namespace Registerations.Test
+{
+    public class RegisterControllerTests
+    {
+        [Fact]
+        public void Post_Method_GivenValidInput_Expect_ValidMessage()
+        {
+            var service = NSubstitute.Substitute.For<IRegisterationService>();
+            var controller = new RegisterController(service);
+            var registerationInfo = new RegisterationInfo();
+
+            var result = controller.Post(registerationInfo);
+
+            service.Received().Save(Arg.Any<RegisterationInfo>());
+            result.GetType().ShouldBe(typeof(OkResult));
+        }
+
+        [Fact]
+        public void Post_Method_GivenInvalidInput_Expect_BadRequestAsResult()
+        {
+            var service = NSubstitute.Substitute.For<IRegisterationService>();
+            var controller = new RegisterController(service);
+            var registerationInfo = new RegisterationInfo();
+            controller.ModelState.AddModelError(nameof(RegisterationInfo.AccountName), "fakeError");
+            var result = controller.Post(registerationInfo);
+
+            service.DidNotReceive().Save(Arg.Any<RegisterationInfo>());
+            result.GetType().ShouldBe(typeof(BadRequestObjectResult));
+            var badResult = (BadRequestObjectResult)result;
+            badResult.Value.GetType().ShouldBe(typeof(SerializableError));
+        }
+    }
+}
