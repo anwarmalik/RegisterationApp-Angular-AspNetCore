d5b3c865905da8fd16e4b31b5414dedaaa784243
Anwar <anwmalik@gmail.com>
2018-03-11 18:33:47 +0800

    added model for register view

---
+++ RegisterationApp/ClientApp/app/models/register.info.ts
@@ -0,0 +1,11 @@
+﻿export class RegistrationInfo {
+
+    constructor(
+        public bsb: number,
+        public accountNumber: number,
+        public accountName: string,
+        public reference: string,
+        public amount: number
+    ) { }
+
+}
