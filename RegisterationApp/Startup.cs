cc95aed71584e8d580c171aec67d251b3e980aa0
Anwar <anwmalik@gmail.com>
2018-03-11 18:27:14 +0800

    register services

--- RegisterationApp/Startup.cs
+++ RegisterationApp/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RegisterationApp.Interfaces;
+using RegisterationApp.Services;
 
 namespace RegisterationApp
 {
@@ -22,6 +24,8 @@ namespace RegisterationApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IFileWriterService, FileWriterService>();
+            services.AddScoped<IRegisterationService, RegisterationService>();
             services.AddMvc();
         }
 
